57d58eff5239ea39d84976f53766bc689a80e914
karimOCR <rossikarim@yahoo.fr>
2022-05-19 10:29:01 +0200

    method of second round added

--- models/tours_and_matches_model.py
+++ models/tours_and_matches_model.py
@@ -142,7 +142,7 @@ class Match:
         print(players_table.all())
 
 
-    def get_match_score(self):
+    def add_match_score_first_round(self):
         """Pour recolter le score des matchs du premier round"""
 
         first_round_matches_list = []
@@ -208,16 +208,66 @@ class Match:
 
     def save_db_match_first_round(self):
 
+    def add_match_score_second_round(self):
+        """Pour recolter le score des matchs du second round"""
+
+        second_round_matches_list = []
+        second_round_matches_dict = {}
+        for i in range(0, 3):
+            # print(i,v)
+            # print(lower_players_list[i])
+            joueur_1 = sorted_players_after_first_round_list[i]
+            joueur_2 = sorted_players_after_first_round_list[i + 1]
+            second_round_match = ([joueur_1['id_du_joueur'], joueur_1['Score']],
+                                  [joueur_2['id_du_joueur'], joueur_2['Score']])
+            print(f"joueur_1 est : {joueur_1['Nom']} {joueur_1['Prenom']} avec ELO de {joueur_1['Classement']}")
+            print(f"joueur_2 est : {joueur_2['Nom']} {joueur_2['Prenom']} avec ELO de {joueur_2['Classement']}")
+            print(f"Qui a gagné le match ? \n")
+            informed_match_winner = False
+            wining_player = None
+            while not informed_match_winner:
+                match_winner = input(
+                    f"si c'est le joueur 1, {joueur_1['Nom']} {joueur_1['Prenom']} qui a gagné, Tapez__1 \n"
+                    f"si c'est le joueur 2, {joueur_2['Nom']} {joueur_2['Prenom']} qui a gagné, Tapez__2 \n"
+                    f"si c'est un match nul alors ________________Tapez__3 \n"
+                    f"Tapez 1 ou 2 ou 3 ___: ")
+                print(type(match_winner))
+                if match_winner == str(1):
+                    informed_match_winner = True
+                    print(f"Le joueur 1 {joueur_1['Nom']} {joueur_1['Prenom']} a g agné cette partie\n"
+                          "et se voit attribué  1 Point")
+                    second_round_match[0][1] += 1
+                    print(f" l'id du joueur {second_round_match[0][0]} a {second_round_match[0][1]} point \n"
+                          f"l'id du joueur {second_round_match[1][0]} a {second_round_match[1][1]} point \n")
 
 
+                elif match_winner == str(2):
+                    informed_match_winner = True
+                    print(f"Le joueur 2 {joueur_2['Nom']} {joueur_2['Prenom']} a gagné cette partie\n"
+                          "et se voit attribué  1 Point")
+                    second_round_match[1][1] += 1
+                    print(f" l'id du joueur {second_round_match[0][0]} a {second_round_match[0][1]} point \n"
+                          f"l'id du joueur {second_round_match[1][0]} a {second_round_match[1][1]} point \n")
 
 
+                elif match_winner == str(3):
+                    informed_match_winner = True
+                    print(f"Le joueur 1 et le joueur 2 ont fait match nul \n"
+                          f"{joueur_1['Nom']} {joueur_1['Prenom']} et joueur 2 {joueur_2['Nom']} {joueur_2['Prenom']}\n"
+                          f"se voient chacun(e) attribué  0,5 Point")
+                    second_round_match[0][1] += 0.5
+                    second_round_match[1][1] += 0.5
+                    print(f" l'id du joueur {second_round_match[0][0]} a {second_round_match[0][1]} point \n"
+                          f"l'id du joueur {second_round_match[1][0]} a {second_round_match[1][1]} point \n")
 
-
-
-
-
-
+                else:
+                    print(f"il y a une erreur dans votre saisie, veuillez recommencer")
+            second_round_matches_list.append(second_round_match)
+            match_dict = dict(second_round_match)
+            print(f"Classement du premier tour avec joueur_id et score associé {match_dict}")
+            second_round_matches_dict.update(match_dict)
+        print(second_round_matches_list)
+        print(second_round_matches_dict)
 
 
 
